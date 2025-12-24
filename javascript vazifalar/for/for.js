c8b95a1f9e4e019c3aa1c1209a529c0560cdebc3
Azizbek-02 <azizbekazimjonow0222@gmail.com>
2025-12-24 10:42:02 +0500

    for ga ozgartirish kiritildi

--- javascript vazifalar/for/for.js
+++ javascript vazifalar/for/for.js
@@ -1,8 +1,10 @@
 // for 1
 let q = 6;
 for( let i= 1; i <= q; i++){
-    console.log(q);
+    console.log(i);
 }
+console.log('-----------------1');
+
 
 // for 2
  let w = 3 ;
@@ -10,64 +12,113 @@ let e = 8;
 for(  ; w <= e; w++){
     console.log(w);
 }
+console.log('-----------------2');
+
 
 // for 3
 let r = 11 ;
 let t= 2;
-for(let y=r - 1; y > t ; y--){
+for(let y = r - 1;  y > t;   y--){
     console.log(y);
-}
+} 
+console.log('-----------------3');
+
 
 // for 4
 let u = 18;
+let uo;
 for( o = 1; o <=10; o++){
-    console.log(o,o * u);
+    uo = o * u;
 }
+console.log(uo);
+console.log('-----------------4');
+
 
 // for 5
  let p = 27;
+ let ap;
  for( a = 0.1; a <=1; a += 0.1 ){
-    console.log( (a * p).toFixed(1));
+    ap = a*p;
  }
+    console.log(ap.toFixed(1));
+
+console.log('-----------------5');
 
 // // for 6
 let s = 34;
+let sd;
 for(d=1.2; d <= 2; d += 0.2){
-    console.log( ( d * s).toFixed(1));
-}
+    sd = d*s;
+ }   
+    console.log( sd.toFixed(1));
+
+console.log('-----------------6');
 
 // for 7
-let g = 14;
+let g = 20;
 let fg = 0;
 for(let f= 1; f <= g; f++ ){
-    console.log(f);
     fg= fg+f;
 }
-console.log(`jami sonlar yigindisi = ${fg} `);
+console.log(`1 dan ${g} gacha bolgan jami sonlar yigindisi = ${fg} `);
+console.log('-----------------7');
 
 // for 8
 let j = 9;
 let jk= 1;
-for(k =3; k <= j; k++ ){
-    console.log(k);
+for(k =1; k <= j; k++ ){
     jk= jk*k;    
 }
-console.log(` jami sonlar kopaytmasi = ${jk}`);
+console.log(`1 dan ${j} gacha bolgan jami sonlar kopaytmasi = ${jk}`);
+console.log('-----------------8');
 
 // for 9
-let l = 9;
+let l = 10;
 let lz = 0;
-for(let z = 2; z <= l; z++){
-    console.log(z);
+for(let z = 1; z <= l; z++){
     lz= lz + z * z;
 }
-console.log(lz);
+console.log(` 1 dan ${l} gacha bolgan sonlar kvadratinig yigindisi = ${lz}`);
+console.log('-----------------9');
 
 // for 10
-let x = 4;
+let x = 10;
 let xc = 0;
 for(c = 1; c <=x ; c++ ){
-console.log(c);
+
 xc= xc + 1 / c;
 }
 console.log(xc);
+console.log('-----------------10');
+
+// for 11
+console.log('-----------------11');
+
+
+
+
+// for 12
+let nnn = 2;
+let S=1;
+for(nS = 1; nS <= nnn; nS += 0.1 )  {
+S *= nS;
+}  
+console.log(S.toFixed(2));
+console.log('-----------------12');
+
+// for 13
+ let vv = 20;
+ let Ss = 0;
+ let vx= 1;
+ for(Bb = 1; Bb <= vv; Bb++ ){
+    let hh = (10 + Bb) / 10;
+     Ss += hh * vx;
+    vx = -vx;
+ }
+ console.log(Ss);
+console.log('-----------------13');
+  
+
+
+
+ 
